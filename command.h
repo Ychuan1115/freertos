db889e764f97ebeb7348766eccd73f9e3480feeb
Andy <andy79923@gmail.com>
2013-10-18 23:31:09 +0800

    Add function to process the command cat

--- command.h
+++ command.h
@@ -1,5 +1,5 @@
 
-#define CMDNUM 5
+#define CMDNUM 6
 
-enum Cmd{HELP=0,ECHO,PS,HELLO,LS};
-static char cmdTable[CMDNUM][20]={"help","echo","ps","hello", "ls"};
+enum Cmd{HELP=0,ECHO,PS,HELLO,LS,CAT};
+static char cmdTable[CMDNUM][20]={"help","echo","ps","hello", "ls", "cat"};
--- main.c
+++ main.c
@@ -114,6 +114,40 @@ void echo(char splitInput[][20], int splitNum)
     fio_write(1, "\n", 1);
 }
 
+void cat(char splitInput[][20], int splitNum)
+{
+    int i=1;
+    while(splitInput[i][0] == '-' && i < splitNum)i++;
+    char buff[128];
+    buff[0]='\0';
+    size_t count;
+    for(; i < splitNum; i++)
+    {
+        char path[20]="/romfs/";
+        strcat(path,splitInput[i]);
+        int fd = fs_open(path, 0, O_RDONLY);
+        if(fd<0)
+        {
+            strcat(buff,"cat: ");
+            strcat(buff,splitInput[i]);
+            strcat(buff,": No such file or directory\r\n");
+            fio_write(1,buff,strlen(buff));
+            buff[0]='\0';
+            continue;
+        }
+        do
+        {
+            //Read from /romfs/test.txt to buffer
+            count = fio_read(fd, buff, sizeof(buff));
+
+            //Write buffer to fd 1 (stdout, through uart)
+            fio_write(1, buff, count);
+        } while (count);
+        fio_write(1, "\r", 1);
+    }
+
+}
+
 void hello(char splitInput[][20], int splitNum)
 {
     char *str = "Hello, World!\n";
@@ -185,6 +219,9 @@ void HandleInput(char* input)
             break;
         case LS:
             ls(splitStr,splitNum);
+            break;
+        case CAT:
+            cat(splitStr,splitNum);
             break;
         default:
             fio_write(1, splitStr[0], strlen(splitStr[0]));
